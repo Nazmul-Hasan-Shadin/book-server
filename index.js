38f8f65e82f768b693b82ad0ab995c8740a083c2
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2023-11-04 23:31:14 +0600

    server has connected to mongodb

---
+++ index.js
@@ -0,0 +1,62 @@
+const express= require('express')
+const cors= require('cors')
+const jwt= require('jsonwebtoken')
+require('dotenv').config()
+var cookieParser = require('cookie-parser')
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const app = express();
+const secret= 'iloveyou'
+const port= process.env.PORT || 5000;
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials:true
+}))
+app.use(express.json())
+app.use(cookieParser())
+
+//  mongodb connecetion
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.wjj4omp.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+   
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/',(req,res)=>{
+    res.send('server is running shadin')
+})
+
+
+app.listen(port, ()=>{
+    console.log('book server is running');
+})
